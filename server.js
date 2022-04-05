7e65272ec970ea1fb12b337017ffd2e92edf4c3e
Yassine Khouaja <yassinekhouaja@takiacademyteam.com>
2022-04-05 10:59:53 +0100

    clean the repo

--- server.js
+++ server.js
@@ -6,10 +6,7 @@ import cors from "cors";
 // db and authenticate user
 import connectDB from "./db/connect.js";
 // routers
-import authRouter from "./routes/authRoutes.js";
-import publicationRouter from "./routes/publicationsRoutes.js";
-import commentRouter from "./routes/commentsRoutes.js";
-import banRouter from "./routes/bansRoutes.js";
+
 import configRouter from "./routes/configRoutes.js";
 import typeRouter from "./routes/typeRoutes.js";
 import styleRouter from "./routes/styleRoutes.js";
@@ -17,35 +14,10 @@ import styleRouter from "./routes/styleRoutes.js";
 // middleware
 import errorHandlerMiddleware from "./middleware/error-handler.js";
 import notFoundMiddleware from "./middleware/not-found.js";
-// documentations
-import swaggerJsDoc from "swagger-jsdoc";
-import swaggerUI from "swagger-ui-express";
 
-console.clear();
 dotenv.config();
 
-const options = {
-  definition: {
-    openapi: "3.0.0",
-    info: {
-      title: "Library API",
-      version: "1.0.0",
-      description: "A simple Express Library API",
-    },
-    servers: [
-      {
-        url: "http://localhost:5000",
-      },
-    ],
-  },
-  apis: ["./routes/*.js"],
-};
-
-const specs = swaggerJsDoc(options);
-
 const app = express();
-
-app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(specs));
 
 if (process.env.NODE_ENV !== "production") {
   app.use(morgan("dev"));
@@ -56,11 +28,6 @@ app.use(express.json());
 app.use("/api/v1/config", configRouter);
 app.use("/api/v1/type", typeRouter);
 app.use("/api/v1/style", styleRouter);
-
-// app.use("/api/v1/auth", authRouter);
-// app.use("/api/v1/publications", publicationRouter);
-// app.use("/api/v1/comments", commentRouter);
-// app.use("/api/v1/bans", banRouter);
 
 app.use(notFoundMiddleware);
 
